2018d2a37f184e03e5e4f0d8f099f43814100a6a
Vincent Laude <vincent.laude2@gmail.com>
2022-12-08 15:51:02 +0100

    feat(semantic-release-config): setup config

--- packages/semantic-release-config/__tests__/semantic-release-config.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const semanticReleaseConfig = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(semanticReleaseConfig(), 'Hello from semanticReleaseConfig');
-console.info("semanticReleaseConfig tests passed");
--- packages/semantic-release-config/lib/semantic-release-config.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = semanticReleaseConfig;
-
-function semanticReleaseConfig() {
-    return "Hello from semanticReleaseConfig";
-}
